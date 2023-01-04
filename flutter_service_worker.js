a79536e8a1ff102ce7aecb4ccea002603ef4bbf0
feodor003 <feodor003@gmail.com>
2023-01-04 19:25:59 +0100

    4

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -37,9 +37,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "9566c9f3c0bec2d7f446b1d27b418fc8",
-"/": "9566c9f3c0bec2d7f446b1d27b418fc8",
-"main.dart.js": "ae0c039e2df261a561a940662896603e",
+"index.html": "97842fabc92a4996bad4a6ebbed1ff19",
+"/": "97842fabc92a4996bad4a6ebbed1ff19",
+"main.dart.js": "72b800b180f92418709358ceb6c703de",
 "manifest.json": "f42279028b0d60427c7dd72ad1b99f5b",
 "version.json": "2067ab2dcb637b7b0df8f2434db66f66"
 };
